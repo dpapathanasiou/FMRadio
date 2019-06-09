dce432f52f72f060d022c47c2de588e847fbcbd6
Denis Papathanasiou <denis@papathanasiou.org>
2019-06-09 17:27:12 -0400

    Only change band when the potentiometer (station tuner) has changed

--- FMRadio.ino
+++ FMRadio.ino
@@ -8,10 +8,10 @@ int potPin = 2; // input pin for the potentiometer (station tuner)
 // FM radio stations in NY: http://www.nyradioguide.com/freqlist.htm
 uint16_t stations[] = {8775,8810,8830,8870,8890,8910,8910,8950,8990,9030,9030,9030,9070,9110,9150,9230,9270,9310,9350,9390,9470,9550,9630,9670,9710,9790,9830,9870,9950,10030,10070,10110,10190,10270,10310,10350,10390,10430,10510,10550,10590,10670,10710,10750};
 int dial = 0; // index of stations (0-43)
+int current = 0;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("starting");
   radio.init();
   radio.debugEnable();
   radio.setMono(false);
@@ -23,16 +23,11 @@ int getStation() {
   return (43./1023.) * analogRead(potPin);
 }
 
-void setStation() {
-  dial = getStation();
-  Serial.print("Station number: "); 
-  Serial.print(dial);
-  Serial.print(" which is station: ");
-  Serial.println(stations[dial]);
-  radio.setBandFrequency(RADIO_BAND_FM, stations[dial]);
-}
-
 void loop() {
-  setStation();
+  current = getStation();
+  if (current != dial ) {
+    dial = current;
+    radio.setBandFrequency(RADIO_BAND_FM, stations[dial]);
+  } 
   delay(1000);
 }
